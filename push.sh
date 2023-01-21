648e4f9a2ed3abaa73b168d896cbd404f4cc430d
carson-coder <carsondpool@gmail.com>
2023-01-21 13:31:09 -0500

    uihnsfcks

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ git add .
 git commit -m "$1"
 git push origin main
 git checkout gh-pages
+git pull
 git checkout main -- Web/*
 mv Web/* .
 rmdir Web
@@ -11,3 +12,4 @@ git add .
 git commit -m "Pages: $1"
 git push origin gh-pages
 git checkout main
+git pull
