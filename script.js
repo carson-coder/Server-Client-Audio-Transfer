59d9bfe21799e833651f0f2f3227cb54ff9682f0
carson-coder <carsondpool@gmail.com>
2023-01-24 19:04:28 -0500

    Pages: Finnish

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function play_pause () {
     return label
 }
 
+var settings
+
 function connect() {
     if (connected) {
         console.log("Trying to connect to server while a connection has already started")
@@ -29,6 +31,12 @@ function connect() {
     // Listen for messages
     socket.addEventListener('message', (event) => {
         console.log('Message from server ', event.data);
+        if (event.data.startsWith("Settings")) {
+            settings = JSON.parse(event.data.replace("Settings ", ""))
+            console.log(settings)
+        } else if (event.data.startsWith("Chunk ")) {
+            load_data(settings.channels, settings.sample_rate, settings.length, JSON.parse(event.data.replace("Chunk ", "")))
+        }
     });
     socket.onclose = function() {
         disconnect(true)
@@ -65,4 +73,44 @@ function wait_for_socket(cb) {
     } else {
       cb()
     }
+}
+
+function load_data(channels, sampleRate, length, data) {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// Create an empty three-second stereo buffer at the sample rate of the AudioContext
+audioCtx.sampleRate = sampleRate
+const myArrayBuffer = audioCtx.createBuffer(
+  channels,
+  audioCtx.sampleRate * length,
+  audioCtx.sampleRate
+);
+
+// Fill the buffer with white noise;
+// just random values between -1.0 and 1.0
+for (let channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+  // This gives us the actual array that contains the data
+  const nowBuffering = myArrayBuffer.getChannelData(channel);
+  for (let i = 0; i < myArrayBuffer.length; i++) {
+    // Math.random() is in [0; 1.0]
+    // audio needs to be in [-1.0; 1.0]
+    nowBuffering[i] = data[i] / 2147483647;
+    if (isNaN(nowBuffering[i])) {nowBuffering[i] = 0}
+    console.log(nowBuffering[i])
+  }
+}
+
+// Get an AudioBufferSourceNode.
+// This is the AudioNode to use when we want to play an AudioBuffer
+const source = audioCtx.createBufferSource();
+
+// set the buffer in the AudioBufferSourceNode
+source.buffer = myArrayBuffer;
+
+// connect the AudioBufferSourceNode to the
+// destination so we can hear the sound
+source.connect(audioCtx.destination);
+
+// start the source playing
+source.start();
 }
